2ad4b25fe3f637a00c76973d01275d0b287de434
aiieii <25023109@myrp.edu.sg>
2026-07-16 14:54:48 +0800

    connected to teachers azure DB & public folder w subfolder images & file  style.css

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-//sql connection
-// const connection = mysql.createConnection({
-//  host: 'localhost',
-//  user: 'root',
-//  password: 'root',
-//  database: 'c237_studentlistapp'});
-// connection.connect((err) => {
-//  if (err) {console.error('Error connecting to MySQL:', err);
-//  return;}
-// console.log('Connected to MySQL database');});
+//sql connectionm (use the teachers azure lab connection)
+ const connection = mysql.createConnection({
+  host: 'c237-eaint-mysql.mysql.database.azure.com',
+ user: 'c237_029',
+ password: 'c237029@2026!',
+ database: 'c237_029_teamongkaipeng', 
+ //tells exp serv to only allow a secure, encrypted connection to the onlien azure db
+ssl:{rejectUnauthorized: true}});
+connection.connect((err) => {
+ if (err) {console.error('Error connecting to MySQL:', err);
+ return;}
+console.log('Connected to MySQL database');});
 
 // Set up view engine
 app.set('view engine', 'ejs');
@@ -58,21 +60,10 @@ app.get('/products', (req, res) => {
   });
 });
 
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
-
-
-
-
-
-
-
-
-
 //HI GUYS I MADE THE REGISTER ROUTE//
 //Register Session MiddleWare(Josh)
 app.use(session({
@@ -83,7 +74,6 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
 
 }));
-
 
 //Register GET Route (Josh)
 app.get('/register', (req, res) => {
